6789271d628874feefb78dd24f146c45202b3da3
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-05-17 18:21:07 +1000

    feat: add input

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Flex, Image, Input} from "@chakra-ui/react";
+import SearchInputComponent from "./SearchInput";
 
 const NavbarComponent: React.FC = () => {
 	return (
-		<div>
-			navbar
-		</div>
+		<Flex
+			bg={'white'} h={'44px'} p={'6px 12px'}
+		>
+			<Flex align={'center'}>
+				<Image src={'/images/redditFace.svg'} h={'30px'} />
+				<Image src={'/images/redditText.svg'} h={'46px'} display={{base: 'none', md: 'unset'}} />
+			</Flex>
+			{/*<Directory />*/}
+			<SearchInputComponent />
+			{/*<RightContent />*/}
+		</Flex>
 	);
 };
 
