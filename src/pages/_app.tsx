cf24913611584b8538fcc90d29ed329e81723371
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-05-24 13:20:33 +1000

    feat: modify the homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,8 @@
 import type { AppProps } from 'next/app'
-import {ChakraProvider} from "@chakra-ui/react";
-// import the theme in global
-import {theme} from "../chakra/theme";
-import LayoutComponent from "../components/Layout/Layout";
-// config the global state management
-import {RecoilRoot} from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        <LayoutComponent>
-          <Component {...pageProps} />
-        </LayoutComponent>
-      </ChakraProvider>
-    </RecoilRoot>
+    <Component {...pageProps} />
   )
 }
 
